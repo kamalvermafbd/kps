22c153ffc21319e945dbebb8774e57cc1f90ba92
kamalvermafbd <167846192+kamalvermafbd@users.noreply.github.com>
2026-04-02 23:09:00 +0530

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,5 @@
 export const API_BASE =
-  "https://script.google.com/macros/s/AKfycbyVBeGbRJg-OOudjKp3PpnujhvU2nprBQfLLDVDBrpGRNNXrE_Cwfd52Yj-t7L0HwEH/exec";
+  "https://script.google.com/macros/s/AKfycbzVTR5MTdCF1aCRLr5E0AcoSselGHtaKiPgovEHMrvqpv9oDQ23FLw7SXy54k2NFMM/exec";
 
 export async function apiGet(api: string, params: Record<string, string> = {}) {
   const query = new URLSearchParams({
