4e907a15e11eb98f1ffc3fafcc4fbb1e24e3de3b
kamalvermafbd <167846192+kamalvermafbd@users.noreply.github.com>
2026-04-03 01:24:40 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -94,8 +94,8 @@ export default function Home({ services }: HomeProps) {
       </section>
 
 {/* Intro Video Section */}
-<section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10 lg:py-24">
-  <div className="grid grid-cols-1 lg:grid-cols-2 gap-16 items-start">
+<section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 lg:py-24">
+  <<div className="grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-16 items-start">
     {/* Video */}
     <div className="rounded-3xl overflow-hidden shadow-2xl border border-slate-200">
    <div className="aspect-video lg:h-[520px]">
