cc7c05922d425719560380bcdbe3f2f08a2722ab
kamalvermafbd <167846192+kamalvermafbd@users.noreply.github.com>
2026-04-03 22:14:32 +0530

    Update BlogList.tsx

--- src/pages/BlogList.tsx
+++ src/pages/BlogList.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { motion } from "motion/react";
+import { apiGet } from "../lib/api";
 import { Calendar, ChevronRight, ArrowRight } from "lucide-react";
 import { Link } from "react-router-dom";
 
