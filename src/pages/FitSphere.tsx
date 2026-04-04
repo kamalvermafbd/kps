cf426585081e4fe6f74f3a8416cfb5800849bca4
kamalvermafbd <167846192+kamalvermafbd@users.noreply.github.com>
2026-04-04 21:16:29 +0530

    Update FitSphere.tsx

--- src/pages/FitSphere.tsx
+++ src/pages/FitSphere.tsx
@@ -116,7 +116,10 @@ export default function FitSphere() {
               </div>
             </div>
 
-            {/* right services cards */}
+<h2 className="text-3xl md:text-4xl font-bold text-slate-900 mb-10">
+  Personalized Nutrition Services We Offer
+</h2>
+
             
 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
   <div className={serviceCardClass}>
